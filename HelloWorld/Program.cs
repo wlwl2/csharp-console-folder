baf3db2cdc34a89fe8fdf660adc5b5d4a478b7e5
William Lim <manvinedi@gmail.com>
2020-04-03 16:25:39 +0800

    Update Program.cs

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -3,8 +3,14 @@
 namespace HelloWorld {
     class Program {
     static void Main(string[] args) {
-      MyClass myClass = new MyClass();
-      Console.WriteLine($"Hello World! {myClass.ReturnMessage()}");
+      // MyClass myClass = new MyClass();
+      // Console.WriteLine($"Hello World! {myClass.ReturnMessage()}");
+
+      string testString = "test";
+
+      testString = 4;
+
+      Console.WriteLine(testString);
     }
   }
 }
